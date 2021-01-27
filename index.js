1538c0d06238ecdb63858fd8a654f7f9741246c7
Trong Nhan <trongnhannguyen94@gmail.com>
2021-01-27 22:40:36 +0700

    Change response request

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 app.get("/", (req, res) => {
-  return res.send("Nodejs with aws");
+  return res.send({
+    name: "Nguyen Trong Nhan",
+    email: "nhannt.se1905@gmail.com",
+  });
 });
 app.get("/home", (req, res) => {
-    return res.send("This is homepage");
-  });
+  return res.send("This is homepage");
+});
 app.listen(port, () => {
   console.log(`App listen on port 3000`);
 });
