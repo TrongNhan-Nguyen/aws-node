aaba23fb8e02b8f93fc2062b45c0d81b90d81126
Trong Nhan <trongnhannguyen94@gmail.com>
2021-01-27 22:37:28 +0700

    Deploy successfully

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   return res.send("Nodejs with aws");
 });
+app.get("/home", (req, res) => {
+    return res.send("This is homepage");
+  });
 app.listen(port, () => {
   console.log(`App listen on port 3000`);
 });
